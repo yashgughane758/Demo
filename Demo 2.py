31b68c9a138be35b041e1b434b2900a0b2c9f24e
yashgughane758 <yashgughane56@gmail.com>
2026-03-20 01:20:40 +0530

    Add files via upload

---
+++ Demo 2.py
@@ -0,0 +1,5 @@
+English=int(input("Enter english marks=",))
+Science=int(input("Enter science marks=",))
+Math=int(input("Enter math marks=",))
+avg=(English+Science+Math)/3
+print("Average marks is=",avg)
