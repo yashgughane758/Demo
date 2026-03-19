f27318193dd3e0343e088475a3fe878e7d01eec1
yashgughane758 <yashgughane56@gmail.com>
2026-03-20 01:41:01 +0530

    Add files via upload

---
+++ Demo 4.py
@@ -0,0 +1,6 @@
+Age=int(input("Enter age of person=",))
+Weight=float(input("Enter the weight of person=",))
+if(Age>18 and Weight>45):
+    print("person is Eligible")
+else:
+    print("person is not Eligible")
