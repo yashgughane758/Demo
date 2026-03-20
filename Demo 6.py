b507491f9824f80d3a3537c08c846a45992eac00
Yash Gughane <yashgughane56@gmail.com>
2026-03-21 00:59:48 +0530

    Add files via upload

---
+++ Demo 6.py
@@ -0,0 +1,8 @@
+A=int(input("Enter angle A "))
+B=int(input("Enter angle B "))
+C=int(input("Enter angle c "))
+if(A+B+C==180):
+    print("Angle are satisfied")
+else:
+    print("Angle are not satisfied")
+
