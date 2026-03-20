d9dd33b3c4a795b41a438b56cb7a01cd60dccf52
Yash Gughane <yashgughane56@gmail.com>
2026-03-21 01:04:43 +0530

    Add files via upload

---
+++ Demo 7.py
@@ -0,0 +1,5 @@
+ch=input("Enter any character ")
+if(ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U' or ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u'):
+    print("It is a vowel")
+else:
+    print("It is a consonent")
